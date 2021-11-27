f88a88d4a7b11f68a247b59e8131f7c454eafff6
Ali Ahmed <alimohdapg@gmail.com>
2021-11-27 20:29:21 +0000

    Version 1.01

---
+++ task3/src/main/language/RARSInterface.java
@@ -0,0 +1,50 @@
+package language;
+
+import rars.AssemblyException;
+import rars.SimulationException;
+import rars.api.Options;
+import rars.api.Program;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RARSInterface
+{
+
+    public static void main(String[] args) throws IOException
+    {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+            sb.append("\n");
+        }
+
+        Options op = new Options();
+        op.startAtMain = true;
+        op.maxSteps = 1000000;
+
+        Program p = new Program(op);
+
+        try {
+            p.assembleString(sb.toString());
+        } catch (AssemblyException ae){
+            throw new RuntimeException(ae);
+        }
+
+        p.setup(null, "");
+        // Potentially set memory or registers here
+        try {
+            System.out.println(p.simulate());
+        } catch (SimulationException se){
+            throw new RuntimeException(se);
+        }
+        // check any final register or memory state
+        System.out.println(p.getRegisterValue("a0"));
+
+    }
+
+}
---
+++ task3/src/main/language/Task3.java
@@ -0,0 +1,11 @@
+package language;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class Task3 {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
